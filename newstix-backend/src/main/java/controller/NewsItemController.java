21507b0f50a8c9491fa1fbac82b9967a6616a998
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-04-16 23:21:50 +0200

    start newsitem controller

--- newstix-backend/src/main/java/controller/NewsItemController.java
+++ newstix-backend/src/main/java/controller/NewsItemController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import models.news.NewsItem;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class NewsItemController {
 
-    @GetMapping
+    @GetMapping("/news")
+    public List<NewsItem> getNews() {
+
+    }
 }
