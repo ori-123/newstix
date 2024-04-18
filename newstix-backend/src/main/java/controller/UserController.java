557240a6a41cf252a4cd81d09aa1adbfc6979954
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-04-19 01:40:52 +0200

    create user controller

---
+++ newstix-backend/src/main/java/controller/UserController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import models.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User registeredUser = userService.register(user);
+        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> loginUser(@RequestBody User user) {
+        User loggedInUser = userService.login(user.getUsername(), user.getPassword());
+        if (loggedInUser != null) {
+            return new ResponseEntity<>(loggedInUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable Long userId) {
+        User user = userService.getUserById(userId);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{userId}/preferences")
+    public ResponseEntity<User> updateUserPreferences(@PathVariable Long userId, @RequestBody User updatedUser) {
+        User user = userService.getUserById(userId);
+        if (user != null) {
+            user.setLanguage(updatedUser.getLanguage());
+            user.setCountry(updatedUser.getCountry());
+            user.setCategory(updatedUser.getCategory());
+            user.setTimeframe(updatedUser.getTimeframe());
+            User savedUser = userService.saveUser(user);
+            return new ResponseEntity<>(savedUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
