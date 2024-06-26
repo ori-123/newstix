1bdb8027ad171748f7bb1ae3c3f1670da21f02ed
ori-123 <orirodatos@gmail.com>
2024-06-26 22:25:40 +0200

    modify newstable

--- newstix-frontend/src/components/Table.vue
+++ newstix-frontend/src/components/Table.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, watch } from "vue";
+import { ref, onMounted } from "vue";
 import axios from "axios";
 import { BTable } from 'bootstrap-vue-next';
 
